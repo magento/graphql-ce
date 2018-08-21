1fe963f34d91938fc5b08d6c0aa9fe1e5f1cdb14
AleksLi <aleksliwork@gmail.com>
2018-08-21 21:40:34 +0300

    GraphQL-43: Created coverage of the query

---
+++ app/code/Magento/WishlistGraphQL/Model/Resolver/Wishlist.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\WishlistGraphQL\Model\Resolver;
+
+use Magento\Authorization\Model\UserContextInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\WishlistGraphQL\Model\Resolver\Wishlist\WishlistDataProvider;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\Resolver\ValueFactory;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+
+class Wishlist implements ResolverInterface
+{
+    /**
+     * @var WishlistDataProvider
+     */
+    private $wishlistResolver;
+
+    /**
+     * @var ValueFactory
+     */
+    private $valueFactory;
+
+    /**
+     * @param WishlistDataProvider $wishlistResolver
+     * @param ValueFactory $valueFactory
+     */
+    public function __construct(
+        WishlistDataProvider $wishlistResolver,
+        ValueFactory $valueFactory
+    ) {
+        $this->wishlistResolver = $wishlistResolver;
+        $this->valueFactory = $valueFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) : Value {
+        /** @var ContextInterface $context */
+        if ((!$context->getUserId()) || $context->getUserType() == UserContextInterface::USER_TYPE_GUEST) {
+            throw new GraphQlAuthorizationException(
+                __(
+                    'Current customer does not have access to the resource "%1"',
+                    [\Magento\Customer\Model\Customer::ENTITY]
+                )
+            );
+        }
+
+        try {
+            $data = $this->customerResolver->getCustomerById($context->getUserId());
+            $result = function () use ($data) {
+                return !empty($data) ? $data : [];
+            };
+            return $this->valueFactory->create($result);
+        } catch (NoSuchEntityException $exception) {
+            throw new GraphQlNoSuchEntityException(__('Wish List %1 does not exist.', [$context->getUserId()]));
+        }
+    }
+}
