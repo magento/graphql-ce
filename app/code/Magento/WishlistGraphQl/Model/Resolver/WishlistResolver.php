0f92b3bd825ef99172458b6a2cfcf0a0f33e38b9
David Verholen <david@verholen.com>
2018-10-26 12:42:07 +0200

    GraphQL-43: introduce wishlist graphql module

--- app/code/Magento/WishlistGraphQl/Model/Resolver/WishlistResolver.php
+++ app/code/Magento/WishlistGraphQl/Model/Resolver/WishlistResolver.php
@@ -4,16 +4,13 @@
  * See COPYING.txt for license details.
  */
 declare(strict_types=1);
-
 namespace Magento\WishlistGraphQl\Model\Resolver;
-
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Wishlist\Model\ResourceModel\Wishlist as WishlistResourceModel;
 use Magento\Wishlist\Model\Wishlist;
 use Magento\Wishlist\Model\WishlistFactory;
-
 /**
  * Fetches the Wishlist data according to the GraphQL schema
  */
@@ -23,12 +20,10 @@ class WishlistResolver implements ResolverInterface
      * @var WishlistResourceModel
      */
     private $wishlistResource;
-
     /**
      * @var WishlistFactory
      */
     private $wishlistFactory;
-
     /**
      * @param WishlistResourceModel $wishlistResource
      * @param WishlistFactory $wishlistFactory
@@ -38,7 +33,6 @@ class WishlistResolver implements ResolverInterface
         $this->wishlistResource = $wishlistResource;
         $this->wishlistFactory = $wishlistFactory;
     }
-
     /**
      * @inheritdoc
      */
@@ -50,15 +44,12 @@ class WishlistResolver implements ResolverInterface
         array $args = null
     ) {
         $customerId = $context->getUserId();
-
         /** @var Wishlist $wishlist */
         $wishlist = $this->wishlistFactory->create();
         $this->wishlistResource->load($wishlist, $customerId, 'customer_id');
-
         if (null === $wishlist->getId()) {
             return [];
         }
-
         return [
             'sharing_code' => $wishlist->getSharingCode(),
             'updated_at' => $wishlist->getUpdatedAt(),
