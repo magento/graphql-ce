b4dfb4749d3032c2959ce602c20395dee632c346
Dominic <d.fernando@ism-apac.com>
2019-02-28 00:18:17 +0530

    api test

---
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/AddSimpleProductToCartCusOptionTest.php
@@ -0,0 +1,280 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\GraphQl\Quote;
+
+use Magento\Quote\Model\Quote;
+use Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface;
+use Magento\Robots\Model\Config\Value;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\TestCase\GraphQlAbstract;
+use Magento\TestFramework\ObjectManager;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
+/**
+ * Test for add product with custom option cart
+ */
+class AddSimpleProductToCartCusOptionTest extends GraphQlAbstract
+{
+    /**
+     * @var Bootstrap
+     */
+    protected $objectManager;
+
+    protected function setUp()
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Catalog/_files/product_with_not_req_custom_options.php
+     * @magentoAppIsolation enabled
+     */
+    public function testAddSimpleProductWithMultiCOCheckbox()
+    {
+        $sku = "simple_not_req_custom_option_2";
+        $quote = $this->getQuote();
+        $quoteIdToMaskedId = $this->objectManager->create(QuoteIdToMaskedQuoteIdInterface::class);
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = ObjectManager::getInstance()->get(ProductRepositoryInterface::class);
+        $product = $productRepository->get($sku, false, null, true);
+        $selectedDetails = $this->getSelectedOptionDetails($product, ["checkbox" => 2]);
+        $selectedJsonString = $selectedDetails["jsonString"];
+        $checkWithResponse = $selectedDetails["checkWithResponse"];
+        $maskedQuoteId = $quoteIdToMaskedId->execute((int)$quote->getId());
+        $query = $this->prepareAddProductRequestQuery($maskedQuoteId, $sku, $selectedJsonString);
+        $response = $this->graphQlQuery($query);
+        self::assertArrayHasKey("addSimpleProductsToCart", $response);
+        $responseOptionDetails = $this->processResponseForCustomOption($response);
+        self::assertTrue(count(array_diff_key($responseOptionDetails, $checkWithResponse["checkbox"])) === 0);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Catalog/_files/product_with_not_req_custom_options.php
+     * @magentoAppIsolation enabled
+     */
+    public function testAddSimpleProductWithOneCOCheckbox()
+    {
+        $sku = "simple_not_req_custom_option_2";
+        $quote = $this->getQuote();
+        $quoteIdToMaskedId = $this->objectManager->create(QuoteIdToMaskedQuoteIdInterface::class);
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = ObjectManager::getInstance()->get(ProductRepositoryInterface::class);
+        $product = $productRepository->get($sku, false, null, true);
+        $selectedDetails = $this->getSelectedOptionDetails($product, ["checkbox" => 1]);
+        $selectedJsonString = $selectedDetails["jsonString"];
+        $checkWithResponse = $selectedDetails["checkWithResponse"];
+        $maskedQuoteId = $quoteIdToMaskedId->execute((int)$quote->getId());
+        $query = $this->prepareAddProductRequestQuery($maskedQuoteId, $sku, $selectedJsonString);
+        $response = $this->graphQlQuery($query);
+        self::assertArrayHasKey("addSimpleProductsToCart", $response);
+        $responseOptionDetails = $this->processResponseForCustomOption($response);
+        self::assertTrue(count(array_diff_key($responseOptionDetails, $checkWithResponse["checkbox"])) === 0);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Catalog/_files/product_with_not_req_custom_options.php
+     * @magentoAppIsolation enabled
+     */
+    public function testAddSimpleProductWithOneCOCheckboxDropDown()
+    {
+        $sku = "simple_not_req_custom_option_2";
+        $quote = $this->getQuote();
+        $quoteIdToMaskedId = $this->objectManager->create(QuoteIdToMaskedQuoteIdInterface::class);
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = ObjectManager::getInstance()->get(ProductRepositoryInterface::class);
+        $product = $productRepository->get($sku, false, null, true);
+        $selectedDetails = $this->getSelectedOptionDetails($product, ["checkbox" => 2,"drop_down" => 1]);
+        $selectedJsonString = $selectedDetails["jsonString"];
+        $checkWithResponse = $selectedDetails["checkWithResponse"];
+        $maskedQuoteId = $quoteIdToMaskedId->execute((int)$quote->getId());
+        $query = $this->prepareAddProductRequestQuery($maskedQuoteId, $sku, $selectedJsonString);
+        $response = $this->graphQlQuery($query);
+        self::assertArrayHasKey("addSimpleProductsToCart", $response);
+        $responseOptionDetails = $this->processResponseForCustomOption($response);
+        self::assertTrue(count(array_diff_key($responseOptionDetails, $checkWithResponse["checkbox_drop_down"])) === 0);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Catalog/_files/product_with_not_req_custom_options.php
+     * @magentoAppIsolation enabled
+     */
+    public function testAddSimpleProductWithOneCOCheckboxDropDownRadio()
+    {
+        $sku = "simple_not_req_custom_option_2";
+        $quote = $this->getQuote();
+        $quoteIdToMaskedId = $this->objectManager->create(QuoteIdToMaskedQuoteIdInterface::class);
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = ObjectManager::getInstance()->get(ProductRepositoryInterface::class);
+        $product = $productRepository->get($sku, false, null, true);
+        $selectedDetails = $this->getSelectedOptionDetails($product, ["checkbox" => 2,"drop_down" => 1, "radio" => 1]);
+        $selectedJsonString = $selectedDetails["jsonString"];
+        $checkWithResponse = $selectedDetails["checkWithResponse"];
+        $maskedQuoteId = $quoteIdToMaskedId->execute((int)$quote->getId());
+        $query = $this->prepareAddProductRequestQuery($maskedQuoteId, $sku, $selectedJsonString);
+        $response = $this->graphQlQuery($query);
+        self::assertArrayHasKey("addSimpleProductsToCart", $response);
+        $responseOptionDetails = $this->processResponseForCustomOption($response);
+        self::assertTrue(count(array_diff_key($responseOptionDetails, $checkWithResponse["checkbox_drop_down"])) === 0);
+    }
+
+    /**
+     * @param \Magento\Catalog\Model\Product $product
+     * @param array $selectedTypes
+     * @return array
+     */
+    public function getSelectedOptionDetails(\Magento\Catalog\Model\Product $product, array $selectedTypes): array
+    {
+        $optionIds = $this->getOptionDetails($product);
+        $checkWithResponse = $selectedOption = [];
+        $keyString = implode("_", array_keys($selectedTypes));
+        foreach ($selectedTypes as $type => $selectCount) {
+            foreach ($optionIds[$type] as $optId => $optVArr) {
+                $count = 0;
+                foreach ($optVArr as $optVId => $optString) {
+                    if ($count < $selectCount) {
+                        $selectedOption[] = $optString;
+                        $checkWithResponse[$keyString][$optId][] = $optVId;
+                        $count++;
+                    }
+                }
+            }
+        }
+
+        return [
+           "jsonString" => implode(",", $selectedOption),
+           "checkWithResponse" => $checkWithResponse
+        ];
+    }
+
+    /**
+     * @param $response
+     * @return array
+     */
+    public function processResponseForCustomOption($response): array
+    {
+        $responseOptionDetails = [];
+        if (isset($response["addSimpleProductsToCart"]["cart"]["items"])) {
+            foreach ($response["addSimpleProductsToCart"]["cart"]["items"] as $itemCustomOption) {
+                if (isset($itemCustomOption["customizable_options"])) {
+                    foreach ($itemCustomOption["customizable_options"] as $option) {
+                        foreach ($option["values"] as $optionV) {
+                            $responseOptionDetails[$option["id"]][] = $optionV["value"];
+                        }
+                    }
+                }
+            }
+        }
+
+        return $responseOptionDetails;
+    }
+
+    /**
+     * @param \Magento\Catalog\Model\Product $product
+     * @return array
+     */
+    public function getOptionDetails(\Magento\Catalog\Model\Product $product): array
+    {
+        $customOptions = $product->getOptions();
+        $optionIds = [];
+        foreach ($customOptions as $option) {
+            /** @var \Magento\Catalog\Model\Product\Option $values */
+            $values = $option->getValues();
+            if (is_array($values)) {
+                /** @var \Magento\Catalog\Model\Product\Option|Value $optionValues */
+                foreach ($values as $optionValues) {
+                    $optV = $optionValues->getOptionTypeId();
+                    $optId = $option->getOptionId();
+                    $optionString = '{id: ' . $optId . ", value: \"" . $optV . "\"}";
+                    $optionIds[$option->getType()][$optId][$optV] = $optionString;
+                }
+            }
+        }
+
+        return $optionIds;
+    }
+
+    /**
+     * @return Quote
+     * @throws \Exception
+     */
+    public function getQuote(): Quote
+    {
+        $quote = $this->objectManager->create(Quote::class);
+        $quote->setStoreId(1)
+            ->setIsActive(true)
+            ->setIsMultiShipping(false);
+
+        $this->objectManager->get(\Magento\Quote\Model\QuoteRepository::class)->save($quote);
+
+        /** @var \Magento\Quote\Model\QuoteIdMask $quoteIdMask */
+        $quoteIdMask = $this->objectManager
+            ->create(\Magento\Quote\Model\QuoteIdMaskFactory::class)
+            ->create();
+
+        $quoteIdMask->setQuoteId($quote->getId());
+        $quoteIdMask->setDataChanges(true);
+        $quoteIdMask->save();
+        return $quote;
+    }
+
+    /**
+     * @param string $maskedQuoteId
+     * @param $sku
+     * @param $selectedOption
+     * @return string
+     */
+    private function prepareAddProductRequestQuery(string $maskedQuoteId, $sku, $selectedOption): string
+    {
+        return <<<QUERY
+                mutation {
+                          addSimpleProductsToCart(input: {
+                                cart_id: "{$maskedQuoteId}",
+                                cartItems: {
+                                    data: {
+                                            sku: "{$sku}",
+                                            qty: 1
+                                    },
+                                    customizable_options: [
+                                      {$selectedOption}
+                                    ]
+                              }
+                        })
+                        { 
+                            cart {
+                              items {
+                                id
+                                qty
+                                ... on SimpleCartItem {
+                                  customizable_options {
+                                    id
+                                    label
+                                    type
+                                    is_required
+                                    values {
+                                      id
+                                      label
+                                      value
+                                      price {
+                                        type
+                                        units
+                                        value
+                                      }
+                                    }
+                                  }
+                                }
+                                product {
+                                  sku
+                                }
+                              }
+                            }    
+                          }
+                        }
+QUERY;
+    }
+
+}
---
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/product_with_not_req_custom_options.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+
+/** @var $product \Magento\Catalog\Model\Product */
+$product = $objectManager->create(\Magento\Catalog\Model\Product::class);
+
+$product->setTypeId(
+    'simple'
+)->setAttributeSetId(
+    4
+)->setWebsiteIds(
+    [1]
+)->setName(
+    'Simple Product With not required Custom Options 3'
+)->setSku(
+    'simple_not_req_custom_option_2'
+)->setPrice(
+    10
+)->setMetaTitle(
+    'meta title'
+)->setMetaKeyword(
+    'meta keyword'
+)->setMetaDescription(
+    'meta description'
+)->setVisibility(
+    \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH
+)->setStatus(
+    \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED
+)->setCanSaveCustomOptions(
+    true
+)->setStockData(
+    [
+        'qty' => 100,
+        'is_in_stock' => 1,
+        'manage_stock' => 1,
+    ]
+)->setHasOptions(true);
+
+$options = [
+    [
+        'title' => 'drop_down option',
+        'type' => 'drop_down',
+        'is_require' => false,
+        'sort_order' => 4,
+        'values' => [
+            [
+                'title' => 'drop_down option 1',
+                'price' => 10,
+                'price_type' => 'fixed',
+                'sku' => 'drop_down option 1 sku',
+                'sort_order' => 1,
+            ],
+            [
+                'title' => 'drop_down option 2',
+                'price' => 20,
+                'price_type' => 'fixed',
+                'sku' => 'drop_down option 2 sku',
+                'sort_order' => 2,
+            ],
+        ],
+    ],
+    [
+        'title' => 'radio option',
+        'type' => 'radio',
+        'is_require' => false,
+        'sort_order' => 5,
+        'values' => [
+            [
+                'title' => 'radio option 1',
+                'price' => 10,
+                'price_type' => 'fixed',
+                'sku' => 'radio option 1 sku',
+                'sort_order' => 1,
+            ],
+            [
+                'title' => 'radio option 2',
+                'price' => 20,
+                'price_type' => 'fixed',
+                'sku' => 'radio option 2 sku',
+                'sort_order' => 2,
+            ],
+        ],
+    ],
+    [
+        'title' => 'checkbox option',
+        'type' => 'checkbox',
+        'is_require' => false,
+        'sort_order' => 6,
+        'values' => [
+            [
+                'title' => 'checkbox option 1',
+                'price' => 10,
+                'price_type' => 'fixed',
+                'sku' => 'checkbox option 1 sku',
+                'sort_order' => 1,
+            ],
+            [
+                'title' => 'checkbox option 2',
+                'price' => 20,
+                'price_type' => 'fixed',
+                'sku' => 'checkbox option 2 sku',
+                'sort_order' => 2,
+            ],
+        ],
+    ]
+];
+
+$customOptions = [];
+
+/** @var \Magento\Catalog\Api\Data\ProductCustomOptionInterfaceFactory $customOptionFactory */
+$customOptionFactory = $objectManager->create(\Magento\Catalog\Api\Data\ProductCustomOptionInterfaceFactory::class);
+
+foreach ($options as $option) {
+    /** @var \Magento\Catalog\Api\Data\ProductCustomOptionInterface $customOption */
+    $customOption = $customOptionFactory->create(['data' => $option]);
+    $customOption->setProductSku($product->getSku());
+
+    $customOptions[] = $customOption;
+}
+
+$product->setOptions($customOptions);
+
+/** @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepositoryFactory */
+$productRepository = $objectManager->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+$productRepository->save($product);
