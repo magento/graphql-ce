8a6bea6baad741e704545075183f518a2632f8fe
AleksLi <aleksliwork@gmail.com>
2019-03-08 20:27:59 +0100

    GraphQL-43: Added Multiple Test scenarios for adding items to wishlist

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Wishlist/AddItemToWishlistTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Wishlist/AddItemToWishlistTest.php
@@ -7,11 +7,13 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Wishlist;
 
+use Magento\Catalog\Model\Product;
 use Magento\Integration\Api\CustomerTokenServiceInterface;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
 use Magento\Wishlist\Model\Wishlist;
 use Magento\Wishlist\Model\Item;
 use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class AddItemToWishlistTest extends GraphQlAbstract
 {
@@ -19,17 +21,20 @@ class AddItemToWishlistTest extends GraphQlAbstract
      * @var CustomerTokenServiceInterface
      */
     private $customerTokenService;
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
 
     protected function setUp()
     {
         parent::setUp();
 
         $this->customerTokenService = Bootstrap::getObjectManager()->get(CustomerTokenServiceInterface::class);
+        $this->productRepository = Bootstrap::getObjectManager()->get(ProductRepositoryInterface::class);
     }
 
     /**
-     * Verify the fields of CMS Block selected by identifiers
-     *
      * @magentoApiDataFixture Magento/Wishlist/_files/wishlist.php
      * @throws \Magento\Framework\Exception\AuthenticationException
      * @throws \Exception
@@ -73,6 +78,204 @@ MUTATION;
         $this->assertEquals($wishlist->getName(), $response['addItemsToWishlist']['wishlist']['name']);
         $this->assertEquals($wishlistItem->getData('qty') + 1, $response['addItemsToWishlist']['wishlist']['items'][0]['qty']);
         $this->assertEquals($wishlistItem->getDescription(), $response['addItemsToWishlist']['wishlist']['items'][0]['description']);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Wishlist/_files/wishlist.php
+     * @magentoApiDataFixture Magento/Catalog/_files/multiple_products.php
+     * @throws \Exception
+     */
+    public function testAddMultipleProductsToWishlist()
+    {
+        /** @var Wishlist $wishlist */
+        $wishlist = Bootstrap::getObjectManager()->create(
+            Wishlist::class
+        );
+        $wishlist->loadByCustomerId(1, true);
+        $wishlist->addNewItem( 10);
+        $wishlist->addNewItem( 11);
+        $mutation =
+            <<<MUTATION
+mutation {
+  addItemsToWishlist (
+    input: {
+      skus: ["simple1", "simple2"]
+    }
+  ) {
+    wishlist {
+      items {
+        id
+        description
+        qty
+      }
+      sharing_code
+      updated_at
+      items_count
+      name
+    }
+  }
+}
+MUTATION;
+
+        $response = $this->graphQlQuery($mutation, [], '', $this->getCustomerAuthHeaders('customer@example.com', 'password'));
+        $this->assertEquals($wishlist->getItemsCount(), $response['addItemsToWishlist']['wishlist']['items_count']);
+    }
+
+    /**
+     * No products would be added to wishlist in this case.
+     *
+     * @magentoApiDataFixture Magento/Wishlist/_files/wishlist.php
+     * @throws \Exception
+     * @throws \Magento\Framework\Exception\AuthenticationException
+     */
+    public function testAddNotExistedProductToWishlist()
+    {
+        /** @var Wishlist $wishlist */
+        $wishlist = Bootstrap::getObjectManager()->create(
+            Wishlist::class
+        );
+        $wishlist->loadByCustomerId(1, true);
+        $mutation =
+            <<<MUTATION
+mutation {
+  addItemsToWishlist (
+    input: {
+      skus: ["non-existed-product"]
+    }
+  ) {
+    wishlist {
+      items {
+        id
+        description
+        qty
+      }
+      sharing_code
+      updated_at
+      items_count
+      name
+    }
+  }
+}
+MUTATION;
+        $response = $this->graphQlQuery($mutation, [], '', $this->getCustomerAuthHeaders('customer@example.com', 'password'));
+        $this->assertEquals($wishlist->getItemsCount(), $response['addItemsToWishlist']['wishlist']['items_count']);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Catalog/_files/product_simple.php
+     * @throws \Exception
+     */
+    public function testAddProductToWishlistForNotAuthorizedCustomer()
+    {
+        $mutation =
+            <<<MUTATION
+mutation {
+  addItemsToWishlist (
+    input: {
+      skus: ["simple"]
+    }
+  ) {
+    wishlist {
+      items {
+        id
+        qty
+      }
+      items_count
+    }
+  }
+}
+MUTATION;
+
+        self::expectExceptionMessage(
+            'Cannot get a wish list for the specified Customer ID'
+        );
+        $this->graphQlQuery($mutation);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Wishlist/_files/wishlist.php
+     * @throws \Exception
+     * @throws \Magento\Framework\Exception\AuthenticationException
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function testAddDuplicateProductToWishlist()
+    {
+        /** @var Wishlist $wishlist */
+        $wishlist = Bootstrap::getObjectManager()->create(
+            Wishlist::class
+        );
+        $wishlist->loadByCustomerId(1, true);
+        /** @var Item $wishlistItem */
+        $wishlistItem = $wishlist->getItemCollection()->getFirstItem();
+        $wishlist->addNewItem(1);
+        $wishlist->addNewItem(1);
+        $mutation =
+            <<<MUTATION
+mutation {
+  addItemsToWishlist (
+    input: {
+      skus: ["simple"]
+    }
+  ) {
+    wishlist {
+      items {
+        qty
+      }
+      items_count
+    }
+  }
+}
+MUTATION;
+
+        $response = $this->graphQlQuery($mutation, [], '', $this->getCustomerAuthHeaders('customer@example.com', 'password'));
+        $this->assertEquals($wishlist->getItemsCount(), $response['addItemsToWishlist']['wishlist']['items_count']);
+        $this->assertEquals($wishlistItem->getData('qty') + 1, $response['addItemsToWishlist']['wishlist']['items'][0]['qty']);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Wishlist/_files/wishlist.php
+     * @magentoApiDataFixture Magento/Catalog/_files/products_in_different_stores.php
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Exception
+     */
+    public function testAddProductsToWishListFromDifferentStores()
+    {
+        /** @var Wishlist $wishlist */
+        $wishlist = Bootstrap::getObjectManager()->create(
+            Wishlist::class
+        );
+        $wishlist->loadByCustomerId(1, true);
+        /** @var Item $wishlistItem */
+        $wishlistItem = $wishlist->getItemCollection()->getFirstItem();
+
+        /** @var Product $productSimple1 */
+        $productSimple1 = $this->productRepository->get('simple_1');
+
+        /** @var Product $productSimple2 */
+        $productSimple2 = $this->productRepository->get('simple_2');
+
+        $wishlist->addNewItem($productSimple1);
+        $wishlist->addNewItem($productSimple2);
+        $mutation =
+            <<<MUTATION
+mutation {
+  addItemsToWishlist (
+    input: {
+      skus: ["simple_1", "simple_2"]
+    }
+  ) {
+    wishlist {
+      items {
+        qty
+      }
+      items_count
+    }
+  }
+}
+MUTATION;
+        $response = $this->graphQlQuery($mutation, [], '', $this->getCustomerAuthHeaders('customer@example.com', 'password'));
+        $this->assertEquals($wishlist->getItemsCount(), $response['addItemsToWishlist']['wishlist']['items_count']);
+        $this->assertEquals($wishlistItem->getData('qty') + 1, $response['addItemsToWishlist']['wishlist']['items'][0]['qty']);
     }
 
     /**
