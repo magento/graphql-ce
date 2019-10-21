00be10d7f553fcb076a6f5a9e13616ee65637afd
Lewis Voncken <lewis@experius.nl>
2019-10-21 14:39:19 +0200

    [FEATURE] Added Product OptionLabel Resolver for the ProductInterface and added the resolver to the manufacturer attribute in the graphql schema

---
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Product/OptionLabel.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogGraphQl\Model\Resolver\Product;
+
+use Magento\Catalog\Model\Product;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+/**
+ * Class OptionLabel
+ *
+ * @package Magento\CatalogGraphQl\Model\Resolver\Product
+ */
+class OptionLabel implements ResolverInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+        /** @var Product $product */
+        $product = $value['model'];
+        return $product->getAttributeText($field->getName());
+    }
+}
