a3407ff2cc4d884958283accd3148ee24c55f95c
Lucas Calazans <calazans95@hotmail.com>
2019-11-14 21:42:20 -0300

    Getting the correct wishlist from the user

--- app/code/Magento/WishlistGraphQl/Model/Resolver/WishlistRemoveItemResolver.php
+++ app/code/Magento/WishlistGraphQl/Model/Resolver/WishlistRemoveItemResolver.php
@@ -8,9 +8,10 @@ declare(strict_types=1);
 namespace Magento\WishlistGraphQl\Model\Resolver;
 
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Wishlist\Model\ResourceModel\Item\Collection as WishlistItemCollection;
 use Magento\Wishlist\Model\ResourceModel\Item\CollectionFactory as WishlistItemCollectionFactory;
@@ -18,7 +19,6 @@ use Magento\Wishlist\Model\ResourceModel\Wishlist as WishlistResourceModel;
 use Magento\Wishlist\Model\ResourceModel\Item as WishlistItemResourceModel;
 use Magento\Wishlist\Model\Wishlist;
 use Magento\Wishlist\Model\WishlistFactory;
-use Magento\Framework\Exception\LocalizedException;
 
 /**
  * Remove wishlist items according to the GraphQL schema
@@ -73,6 +73,7 @@ class WishlistRemoveItemResolver implements ResolverInterface
 
     /**
      * @inheritdoc
+     * @throws GraphQlAuthorizationException
      */
     public function resolve(
         Field $field,
@@ -81,14 +82,24 @@ class WishlistRemoveItemResolver implements ResolverInterface
         array $value = null,
         array $args = null
     ) {
-        $wishlist = $this->getWishlistById($args['input']['wishlist_id']);
-        $wishlistId = $wishlist->getId();
+        $customerId = $context->getUserId();
 
-        $this->deleteWishlistItems($wishlist, $args['input']['wishlist_items_ids']);
+        if (!$customerId && $customerId === 0) {
+            throw new GraphQlAuthorizationException(__('The current user cannot perform operations on wishlist'));
+        }
+
+        $wishlist = $this->getWishlistById($args['input']['wishlist_id']);
+
+        if ((int) $wishlist->getCustomerId() !== $customerId) {
+            throw new GraphQlAuthorizationException(__('The current user doesn\'t have a wishlist with the provided id'));
+        }
+
+        $storeId = (int) $context->getExtensionAttributes()->getStore()->getId();
+        $this->deleteWishlistItems($wishlist, $args['input']['wishlist_items_ids'], $storeId);
 
         return [
             'wishlist' => [
-                'id' => $wishlistId,
+                'id' => $wishlist->getId(),
                 'sharing_code' => $wishlist->getSharingCode(),
                 'updated_at' => $wishlist->getUpdatedAt(),
                 'items_count' => $wishlist->getItemsCount(),
@@ -113,37 +124,42 @@ class WishlistRemoveItemResolver implements ResolverInterface
     }
 
     /**
+     * Delete wishlist items by an array of wishlist items ids
+     *
      * @param Wishlist $wishlist
      * @param array $ids
-     * @throws LocalizedException
+     * @param int $storeId
+     * @throws GraphQlInputException
      */
-    private function deleteWishlistItems(Wishlist $wishlist, Array $ids)
+    private function deleteWishlistItems(Wishlist $wishlist, Array $ids, $storeId)
     {
-        $wishlistItems = $this->getWishListItems($wishlist, $ids);
+        $wishlistItems = $this->getWishListItems($wishlist, $ids, $storeId);
 
         foreach ($wishlistItems as $whislistItem) {
             try {
                 $this->wishlistItemResource->delete($whislistItem);
             } catch (\Exception $e) {
-                throw new LocalizedException(__('Failed to delete the wishlist item'));
+                throw new GraphQlInputException(__('Failed to delete the wishlist item'));
             }
         }
     }
 
     /**
+     * Get wishlist items by an array of wishlist items ids
+     *
      * @param Wishlist $wishlist
      * @param array $ids
+     * @param int $storeId
      * @return array
      */
-    private function getWishListItems(Wishlist $wishlist, Array $ids): array
+    private function getWishListItems(Wishlist $wishlist, Array $ids, $storeId): array
     {
         /** @var WishlistItemCollection $wishlistItemCollection */
         $wishlistItemCollection = $this->wishlistItemCollectionFactory->create();
+
         $wishlistItemCollection
             ->addWishlistFilter($wishlist)
-            ->addStoreFilter(array_map(function (StoreInterface $store) {
-                return $store->getId();
-            }, $this->storeManager->getStores()))
+            ->addStoreFilter($storeId)
             ->setVisibilityFilter()
             ->addFieldToFilter('wishlist_item_id', array('in' => $ids));
 
